4933528c1973592e723035f68d4f5af287f1939f
Allison <amh03160@gmail.com>
2022-03-04 10:43:23 -0600

    renders mountain details when clicked from list

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const mtnDetails = document.querySelector("#mountain-details");
 const mountainList = document.querySelector(`#mountains-list`);
 const likeBttn= document.querySelector(`#like-button`); 
 const mtnLikes = document.querySelector(`#mountain-likes`)
-
-
+const imgBox = document.querySelector(`#mountain-image`);
+const locationBox = document.querySelector(`#mountain-location`);
+const nameBox = document.querySelector(`#mountain-name`);
+const baseURL = `http://localhost:3000/mountains`
 //fetch data 
-fetch(`http://localhost:3000/mountains`)
+fetch(baseURL)
 .then(mountains => mountains.json())
 .then(mtnArray => renderMountainList(mtnArray));
 
@@ -17,10 +19,11 @@ fetch(`http://localhost:3000/mountains`)
 function renderMountainList(mtnArray){
     //create element
     for (mtn of mtnArray){
-    //debugger;
     const mtnLi = document.createElement("li");
     mtnLi.innerText = mtn.name;
-    mtnLi.addEventListener(`click`,() => renderDetails(mtn))
+    mtnLi.mtnId = mtn.id;
+    mtnLi.addEventListener(`click`,(e) => renderDetails(e));
+   // debugger;
     mountainList.appendChild(mtnLi);
     }
     console.log(mtnArray);
@@ -28,9 +31,21 @@ function renderMountainList(mtnArray){
 };
 
 //CLICK ON MOUNTAIN AND SEE DETAILS
-function renderDetails(mtnObj){
+function renderDetails(e){
+fetch(`${baseURL}/${e.target.mtnId}`)
+.then(mtn => mtn.json())
+.then(mtnObj => {
     console.log(mtnObj);
-
+   // debugger;
+    //update pic
+    imgBox.src = mtnObj.image;
+    //update mtn name
+    nameBox.innerText = mtnObj.name;
+    //update mtn location
+    locationBox.innerText = mtnObj.location;
+    //display likes
+    mtnLikes.innerText = mtnObj.likes;
+});
 }
 
 //LIKE BUTTON INCREMENTS BY ONE
